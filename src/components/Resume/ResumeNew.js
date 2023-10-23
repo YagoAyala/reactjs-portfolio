c38edc0f022a1e5d02b750c5b7a499ce6ae0d1a7
Yago Costa Ayala <77995791+YagoAyala@users.noreply.github.com>
2023-10-22 21:21:13 -0300

    chore: changed pdf name

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/yago_costa_ayala_curriculum.pdf";
+import pdf from "../../Assets/../Assets/Yago_Costa_Ayala_Curriculo.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink = "https://raw.githubusercontent.com/yagoayala/reactjs-portfolio/main/src/Assets/yago_costa_ayala_curriculum.pdf";
+const resumeLink =
+  "https://raw.githubusercontent.com/yagoayala/reactjs-portfolio/main/src/Assets/Yago_Costa_Ayala_Curriculo.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
